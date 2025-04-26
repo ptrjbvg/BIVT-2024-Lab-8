9e730d5693be19d0c260a97e3550905ea0d1419b
ptrjbvg <m2414154@edu.misis.ru>
2025-04-26 22:28:41 +0300

    Update Blue_4.cs

--- Blue_4.cs
+++ Blue_4.cs
@@ -1,49 +1,52 @@
 using System;
 
-public class Blue_4 : Blue
+namespace Lab_8
 {
-    private int _output = 0;
-    public int Output => _output;
-
-    public Blue_4(string input) : base(input) {}
-
-    public override void Review()
+    public class Blue_4 : Blue
     {
-        int sum = 0;
-        string current = "";
-        foreach (char c in Input)
+        private int _output = 0;
+        public int Output => _output;
+
+        public Blue_4(string input) : base(input) {}
+
+        public override void Review()
         {
-            if (char.IsDigit(c))
+            int sum = 0;
+            string current = "";
+            foreach (char c in Input)
             {
-                current += c;
-            }
-            else
-            {
-                if (current != "")
+                if (char.IsDigit(c))
                 {
-                    sum += ManualParse(current);
-                    current = "";
+                    current += c;
+                }
+                else
+                {
+                    if (current != "")
+                    {
+                        sum += ManualParse(current);
+                        current = "";
+                    }
                 }
             }
+
+            if (current != "")
+            {
+                sum += ManualParse(current);
+            }
+
+            _output = sum;
         }
 
-        if (current != "")
+        private int ManualParse(string num)
         {
-            sum += ManualParse(current);
+            int result = 0;
+            foreach (char c in num)
+            {
+                result = result * 10 + (c - '0');
+            }
+            return result;
         }
 
-        _output = sum;
+        public override string ToString() => Output.ToString();
     }
-
-    private int ManualParse(string num)
-    {
-        int result = 0;
-        foreach (char c in num)
-        {
-            result = result * 10 + (c - '0');
-        }
-        return result;
-    }
-
-    public override string ToString() => Output.ToString();
 }
